6f58f56a62ee8b34d678cfe046cb063a7064573c
NAHOM BEKELE <nahombekele24@gmail.com>
2026-01-10 00:21:40 +0300

    Refactor AuthService class structure

--- Backend/services/authServices.js
+++ Backend/services/authServices.js
@@ -2,13 +2,11 @@ const db = require("../config/db_config");
 const bcrypt = require("bcryptjs");
 
 class AuthService {
-    // Check if user exists
     async findUserByEmail(email) {
         const [rows] = await db.query("SELECT * FROM Members WHERE email = ?", [email]);
         return rows[0];
     }
 
-    // Hash password and save user
     async createMember(userData) {
         const { first_name, last_name, date_of_birth, email, password, phone, street_address, plan_id } = userData;
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -19,17 +17,16 @@ class AuthService {
         return await db.query(sql, [first_name, last_name, date_of_birth, email, hashedPassword, phone, street_address, plan_id]);
     }
 
-    // Get specific member details
     async getMemberById(userId) {
         const [rows] = await db.query("SELECT * FROM Members WHERE id = ?", [userId]);
         return rows[0];
     }
 
-    // Get all users for admin
     async getAllUsers() {
         const [rows] = await db.query("SELECT id, first_name, last_name, email, department, role FROM Members");
         return rows;
     }
 }
+
 
 module.exports = new AuthService();
